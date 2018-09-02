d0cf2d786711d8da547189ec7c7611da406126de
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-09-02 15:48:30 -0400

    comvinacion de 2 o mas reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,7 @@ import { TodosItemComponent } from './todo/todos-item/todos-item.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 import { StoreModule } from '@ngrx/store';
-import { AppState } from './app.reducers';
-import { todoReducer } from './todo/todo.reducer';
+import { AppReducers } from './app.reducers';
 import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
@@ -27,7 +26,7 @@ import {ReactiveFormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    StoreModule.forRoot({ todos: todoReducer}),
+    StoreModule.forRoot(AppReducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,5 +1,15 @@
 import { Todo } from './todo/model/todo.model';
+import { ActionReducerMap } from '@ngrx/store';
+import * as fromTodo from './todo/todo.reducer';
+import * as fromFiltro from './filter/filter.reducer';
+import * as fromFiltroACtion from './filter/filter.actions';
 
 export interface AppState {
     todos: Todo[];
+    filtro: fromFiltroACtion.filtrosValidos;
 }
+
+export const AppReducers: ActionReducerMap<AppState> = {
+    todos: fromTodo.todoReducer,
+    filtro: fromFiltro.filtroReducer
+};
