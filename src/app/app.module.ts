8555be7c81aa2478c4ed2c21320a23eda2dea174
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-09-02 14:07:37 -0400

    definiendo app state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { TodoComponent } from './todo/todo.component';
@@ -8,6 +9,9 @@ import { TodosListComponent } from './todo/todos-list/todos-list.component';
 import { TodosItemComponent } from './todo/todos-item/todos-item.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { StoreModule } from '@ngrx/store';
+import { AppState } from './app.reducers';
+import { todoReducer } from './todo/todo.reducer';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,12 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
     TodoAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ todos: todoReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
